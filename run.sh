855fe2d6aafab87e5a168d020acd797a01d5e95f
BeanLiu <njueebeanliu@hotmail.com>
2015-12-13 17:02:50 +0800

    push to github and test it again

--- run.sh
+++ run.sh
@@ -4,10 +4,8 @@ cd ~
 
 rm -rf AutoConfigMyUbuntuServer
 
-apt-get update & wait
+apt-get update && apt-get install git -y && git clone https://github.com/BeanLiu1994/AutoConfigMyUbuntuServer.git
 
-apt-get install git -y & wait
-git clone https://github.com/BeanLiu1994/AutoConfigMyUbuntuServer.git
 cd AutoConfigMyUbuntuServer
 
 chmod +x *.sh
